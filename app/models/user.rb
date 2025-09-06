a922c04a02004f7a8dc678194e4207a57194a741
atlmetal <mateodlr90@gmail.com>
2025-09-06 11:07:30 -0500

    create user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :role, inclusion: { in: %w[user admin] }
+
+  def admin?
+    role == 'admin'
+  end
+
+  def can_create_orders?
+    %w[user admin].include?(role)
+  end
 end
