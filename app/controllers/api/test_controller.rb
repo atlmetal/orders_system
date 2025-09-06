ac5779dc4ddbeb92df041815a88b7ddb93d733ff
atlmetal <mateodlr90@gmail.com>
2025-09-06 12:32:16 -0500

    create test controller and test chain file

---
+++ app/controllers/api/test_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  class TestController < ApplicationController
+    def users
+      users = User.all.map do |user|
+        {
+          email: user.email,
+          role: user.role,
+          can_create_orders: user.can_create_orders?
+        }
+      end
+
+      render json: { users: users }
+    end
+
+    def reset_cache
+      Rails.cache.clear
+      render json: { message: "Cache cleared" }
+    end
+
+    def reset_brute_force
+      Rails.cache.delete_matched("failed_attempts:*")
+      render json: { message: "Brute force attempts cleared" }
+    end
+  end
+end
